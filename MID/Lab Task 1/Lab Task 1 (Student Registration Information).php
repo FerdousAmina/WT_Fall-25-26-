aa9f789248a8fb47b2b66453416acbb7ed672391
Amina <aminaferdous1120@gmail.com>
2025-11-06 23:06:01 +0600

    Initial commit for Web Tech Project

---
+++ MID/Lab Task 1/Lab Task 1 (Student Registration Information).php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Student Registration Info</title>
+</head>
+
+</body>
+<h1 style="color:red;border-bottom: 3px solid red; width: 33%;">Student Registration Information</h1>
+
+First name<br>
+<input type="text"><br><br>
+Last name<br>
+<input type="text"><br><br>
+Student ID<br>
+<input type="text"><br><br>
+Program/Major <br>
+<select id="program_major" name="program_major">
+<option value="cs">CSE</option>
+<option value="ee">EEE</option>
+<option value="math">LLB</option>
+<option value="is">Pharmacy</option>
+</select>
+<br><br>
+
+Phone<br>
+<input type="text"><br><br>
+University Email<br>
+<input type="text"><br><br>
+Create Password (min 8 characters)<br>
+<input type="text"><br><br>
+Confirm Password<br>
+<input type="text"><br><br>
+
+Semester Selection <br>
+<input type="radio"> Summer 2025
+<input type="radio"> Fall 2025
+<input type="radio"> Spring 2025
+<input type="radio"> Other/Special Term
+<br><br>
+
+Requested Credit Load (Max 15)<br>
+<input type="text"><br><br>
+<input type="checkbox"> I require academic advising before final registration.
+<p style="color:red; font-size:25px;border-bottom: 3px solid red; width: 13%;">Course Selection</p>
+<input type="checkbox"> Web Technologies
+<input type="checkbox"> Data Science
+<input type="checkbox"> Data Mining
+<input type="checkbox"> Machine Learning
+<br><br>
+
+Comments/Special Requests<br>
+<input type="text"style="width:450px;height:80px;"><br><br>
+
+<center><input type="submit"></center><br>
+</body>
+
+</html>
+</body>
+</html>
