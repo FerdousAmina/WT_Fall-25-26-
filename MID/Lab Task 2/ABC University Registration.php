d956a5cce035b0b7e9854f7bf9f4ca9715f89332
Amina <aminaferdous1120@gmail.com>
2025-11-17 12:08:42 +0600

    commit for LT 2

--- MID/Lab Task 2/ABC University Registration.php
+++ MID/Lab Task 2/ABC University Registration.php
@@ -6,6 +6,8 @@
 
 <body>
 <center><h1 style="color:blue;border-bottom: 3px solid blue; width: 33%;">Student Registration</h1></center>
+
+<form id="Registration Form">
 Full name:<br>
 <input type="text"><br><br>
 <body>
@@ -15,9 +17,18 @@ Password:<br>
 <input type="text"><br><br>
 Confirm Password:<br>
 <input type="text"><br><br>
-
-
 <input type="Submit"><Register><br>
+</form>
+
+<script>
+    function handleSubmit() {
+      var FullName = document.getElementById("FullName").value.trim();
+      var Email = document.getElementById("Email").value.trim();
+      var Password = document.getElementById("Password").value;
+      var ConfirmPassword = document.getElementById("ConfirmPassword").value;
+      
+      errorDiv.innerHTML = "";
+      outputDiv.innerHTML = "";
 
 </body>
 </html>
