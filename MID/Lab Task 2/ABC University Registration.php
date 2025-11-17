7f6577dd508ca2d1ba939357554fbee8c93be11b
Amina <aminaferdous1120@gmail.com>
2025-11-17 11:39:35 +0600

    commit for LT 2

--- MID/Lab Task 2/ABC University Registration.php
+++ MID/Lab Task 2/ABC University Registration.php
@@ -5,9 +5,16 @@
 </head>
 
 <body>
+<h1 style="color:blue;border-bottom: 3px solid blue; width: 33%;">Student Registration</h1>
 Full name:<br>
 <input type="text"><br><br>
-
+<body>
+Email:<br>
+<input type="text"><br><br>
+Password:<br>
+<input type="text"><br><br>
+Confirm Password:<br>
+<input type="text"><br><br>
 </body>
 </html>
 
