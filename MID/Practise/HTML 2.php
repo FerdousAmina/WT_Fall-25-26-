45a64fc3e251cc3211000a3f4f52459cb3eba667
Amina <aminaferdous1120@gmail.com>
2025-11-30 23:13:07 +0600

    commit for HTML LT 2

---
+++ MID/Practise/HTML 2.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>University Online Admission Form</title>
+</head>
+<body>
+
+    <h1>University Online Admission Form</h1>
+
+    <form action="/submit-admission" method="post">
+        
+        <fieldset>
+            <legend>Personal Information</legend>
+            
+            <section>
+                <label for="Name">Full Name:</label>
+                <input type="text" id="Name" name="Name" maxlength="100" required>
+            </section>
+
+            <section>
+                <label for="email">Email Address:</label>
+                <input type="email" id="email" name="email" required>
+            </section>
+
+            <section>
+                <label for="dob">Date of Birth:</label>
+                <input type="date" id="dob" name="dob" required>
+            </section>
+
+            <section>
+                    <input type="radio" id="genderMale" name="gender" value="Male" required>
+                    <label for="genderMale">Male</label>
+
+                    <input type="radio" id="genderFemale" name="gender" value="Female">
+                    <label for="genderFemale">Female</label>
+
+                    <input type="radio" id="genderOther" name="gender" value="Other">
+                    <label for="genderOther">Other</label>
+                </section>
+            
+        </fieldset>
+
+        <fieldset>
+            <legend>Academic Information</legend>
+            
+            <section>
+                <label for="lastInstitution">Last Attended Institution:</label>
+                <input type="text" id="lastInstitution" name="lastInstitution" required>
+            </section>
+
+            <section>
+                <label for="gpa">GPA/Result (Last Exam):</label>
+                <input type="text" id="gpa" name="gpa" required>
+            </section>
+
+            <section>
+                <label for="preferredDept">Preferred Department/Program:</label>
+                <select id="preferredDept" name="preferredDept" required>
+                    <option value="CSE">Computer Science & Engineering (CSE)</option>
+                    <option value="EEE">Electrical & Electronic Engineering (EEE)</option>
+                    <option value="BBA">Bachelor of Business Administration (BBA)</option>
+                    <option value="ENG">English</option>
+                    <option value="ARC">Architecture</option>
+                </select>
+            </section>
+
+        </fieldset>
+
+        <fieldset>
+            <legend>Required Documents</legend>
+            
+            <section>
+                <label for="transcript">Upload Academic Transcript/Marksheet: </label>
+                <input type="file" id="transcript" name="transcript">
+            </section>
+
+            <section>
+                <label for="photo">Upload Passport Size Photo: </label>
+                <input type="file" id="photo" name="photo" >
+            </section>
+
+        </fieldset>
+
+        <section>
+            <button type="reset">Reset Form</button>
+            <button type="submit">Submit Application</button>
+        </section>
+
+    </form>
+
+</body>
+</html>
